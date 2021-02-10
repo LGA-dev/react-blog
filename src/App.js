c8e164f305b9060b4ad5a0d120551a6eee285535
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-02-10 11:33:31 -0300

    finish part 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  const link = "https://www.google.com";
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App component</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        <p>{ 10 }</p>
+        <p>{ "test" }</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random() * 10 }</p>
+        <a href={ link }>Google site</a>
       </div>
     </div>
   );
