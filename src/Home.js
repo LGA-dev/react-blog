d77dbda937be13a38dd921a42d0c8b053a421731
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-02-14 18:20:09 -0300

    finish part 7

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
 const Home = () => {
+
+  const handleClick = (e) => {
+    console.log('hello', e);
+  }
+
+  const handleClickAgain = (name) => {
+    console.log('hello ' + name);
+  }
+
   return (
     <div className="home">
       <h2>HomePage</h2>
+      <button onClick={ handleClick }>Click me</button>
+      {/* Long version */}
+      <button onClick={ () => {handleClickAgain("john")} }>Click me again</button>
+      {/* Short version */}
+      <button onClick={ () => handleClickAgain("john") }>Click me again</button> 
     </div>
   );
 }
