c173d65137121a94144ea738dd16d2e1257139b4
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-02-15 23:30:42 -0300

    finish part 8

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,19 @@
+import { useState } from 'react'
+
 const Home = () => {
+  const [name, setName] = useState("john")
+  const [age, setAge] = useState(18)
 
-  const handleClick = (e) => {
-    console.log('hello', e);
-  }
-
-  const handleClickAgain = (name) => {
-    console.log('hello ' + name);
+  const handleClick = () => {
+    setName("paul")
+    setAge(30)
   }
 
   return (
     <div className="home">
       <h2>HomePage</h2>
+      <p>{ name } is { age } years old</p>
       <button onClick={ handleClick }>Click me</button>
-      {/* Long version */}
-      <button onClick={ () => {handleClickAgain("john")} }>Click me again</button>
-      {/* Short version */}
-      <button onClick={ () => handleClickAgain("john") }>Click me again</button> 
     </div>
   );
 }
