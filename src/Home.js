8613be6af7604fc56cf3ce4a80de4c4fcf97a9a5
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-04-15 08:27:12 -0300

    Code cleanup

--- src/Home.js
+++ src/Home.js
@@ -2,13 +2,7 @@ import { useState, useEffect } from 'react'
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'john', id: 1},
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'paul', id: 2},
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'ringo', id: 3},
-  ]);
-
-  const [name, setName] = useState('george');
+  const [blogs, setBlogs] = useState(null);
 
   const handleDelete = (id) => {
     const newBlog = blogs.filter(blog => blog.id !== id);
@@ -17,16 +11,11 @@ const Home = () => {
 
   useEffect(() => {
     console.log('use effect ran');
-    // console.log(blogs);
-    console.log(name)
-  }, [name]);
+  }, []);
 
   return (
     <div className="home">
-      {/* <BlogList blogs={blogs.filter((blog) => blog.author === "john")} title="John's blogs"/> */}
       <BlogList blogs={blogs} title="New Blogs" handleDelete={handleDelete}/>
-      {/* <button onClick={() => setName('john')}>Change name</button>
-      <p>{ name }</p> */}
     </div>
   );
 }
