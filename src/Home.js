bfbfe8a4c81ff7ce1071f622ab708b1706cfb2b7
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-04-20 08:58:15 -0300

    finish part 18,19

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,33 @@ import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   // Can't use async/await inside useEffect [ useEffect(async () => { ]
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       // Return a promise
       .then(response => {
+        // Response object
+        // console.log(response)
+        if(!response.ok) {
+          throw Error('could not fetch the data for that resource :(')
+        }
         // Returning this will make this return another promise, which will be used in the .then below
         return response.json()
       })
       .then(data => {
         // console.log(data);
         setBlogs(data);
-      });
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch(err => {
+        // console.log(err.message);
+        setIsLoading(false);
+        setError(err.message);
+      })
   }, []);
 
   return (
@@ -29,6 +43,8 @@ const Home = () => {
       2. fetch the blogs and set them
       3. blogs = true
       4. render the component */}
+      {error && <div>{ error }</div>}
+      {isLoading && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="New Blogs" />}
     </div>
   );
